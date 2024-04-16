44dc38bf67c2158ca262a5a05dd619f980b94342
Thanushie <161690799+Thanushie@users.noreply.github.com>
2024-04-16 15:32:21 +1000

    sprint 1 task 2 and 3 update
    added some user input validations for the add member form .need to do some further improvements and check the validations.
    
    Created the editstaff page

--- src/AddMember.php
+++ src/AddMember.php
@@ -34,7 +34,7 @@
         <input type="text" name="fcontact" id="fcontact"> <br><br>
 
         <label class="label" for="medhistory"> Medical History: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-        <input type="text" name="medhistory" id="medhistory"> <br><br>
+        <textarea name="medhistory" id="medhistory" maxlength="260" width="300px" style="width: 300px; height: 100px;"></textarea> <br><br>
 
         <label class="label" for="bpy"> Billing Per Year: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
         <input type="text" name="bpy" id="bpy"> <br><br>
@@ -47,8 +47,95 @@
 
 <?php
 
+$pattern1 = "/[0-9]*$/"; //validation pattern for only numbers
+$pattern2 = "/[a-zA-Z]*$/"; // validation pattern for only alphanumerics
+$pattern3 = "/[\w]+(@)[a-zA-Z]+?(\.[a-zA-Z]+)+/"; // validation pattern for email 
+
 if(isset($_POST["submit"]))
 {
+
+$checkerror = 0;    
+$memberId = $_POST["memberId"];
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$dob = $_POST["dob"];
+$contact = $_POST["contact"];
+$fcontact = $_POST["fcontact"];
+$medhistory = $_POST["medhistory"];
+$bpy = $_POST["bpy"];
+
+//id validation
+if(empty($memberId))
+{
+    echo "Member ID cannot be empty.<br>";
+    $checkerror = $checkerror +1;
+}
+elseif(preg_match($pattern1, $memberId) == "false")
+{
+    echo "Member ID can only contain digits between 0-9.<br>";
+    $checkerror = $checkerror +1;
+}
+
+//first name validation
+if(empty($fname))
+{
+    echo "First name cannot be empty.<br>";
+    $checkerror = $checkerror +1;
+}
+elseif(preg_match($pattern2, $fname) == "false")
+{
+    echo "First can only contain alphanumeric characters.<br>";
+    $checkerror = $checkerror +1;
+}
+
+//last name validation
+if(empty($lname))
+{
+    echo "Last name cannot be empty.<br>";
+    $checkerror = $checkerror +1;
+}
+elseif(preg_match($pattern1, $lname) == "false")
+{
+    echo "Last name can only contain alphanumeric characters.<br>";
+    $checkerror = $checkerror +1;
+}
+
+//date of birth validation
+if(empty($dob))
+{
+    echo "Date of birth cannot be empty.<br>";
+    $checkerror = $checkerror +1;
+}
+elseif(preg_match($pattern3, $dob) == "false")
+{
+    echo "Date must be in dd/mm/yy format.<br>";
+    $checkerror = $checkerror +1;
+}
+
+//contact validation
+if(empty($contact))
+{
+    echo "Contact email cannot be empty.<br>";
+    $checkerror = $checkerror +1;
+}
+elseif(preg_match($pattern3, $contact) == "false")
+{
+    echo "email must be in name123@example.com format.<br>";
+    $checkerror = $checkerror +1;
+}
+
+//fcontact validation
+if(empty($fcontact))
+{
+    echo "family Contact cannot be empty.<br>";
+    $checkerror = $checkerror +1;
+}
+elseif(preg_match($pattern3, $fcontact) == "false")
+{
+    echo "email must be in name123@example.com format.<br>";
+    $checkerror = $checkerror +1;
+}
+
 // Database connection details
 $host = "db";
 $port = "3306";
@@ -67,14 +154,7 @@ if ($mysqli->connect_errno)
 }
     // ## 2. set up SQL string and execute
 
-    $memberId = $_POST["memberId"];
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $dob = $_POST["dob"];
-    $contact = $_POST["contact"];
-    $fcontact = $_POST["fcontact"];
-    $medhistory = $_POST["medhistory"];
-    $bpy = $_POST["bpy"];
+
 
     $query = "INSERT INTO Members (Id, FirstName, LastName, DateOfBirth, Contact, FamilyContact, MedicalHistory, BillingPerYear) VALUES
     ('$memberId', '$fname', '$lname', '$dob', '$contact', '$fcontact', '$medhistory','$bpy')";
