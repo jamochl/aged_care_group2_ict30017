3369ad81dd5903c11997d20d4a0e385f012915b3
miguel <102767226@student.swin.edu.au>
2024-04-29 09:24:13 +1000

    test

--- src/forgot_password.php
+++ src/forgot_password.php
@@ -45,11 +45,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php else : ?>
             <form method="post" action="#">
                 <div class="mb-3">
-                    <label for="username" class="form-label">Username:</label>
-                    <input type="text" class="form-control" id="username" name="username" value="<?php echo isset($username) ? $username : ''; ?>" readonly>
+                    <label for="Username" class="form-label">Username:</label>
+                    <input type="text" class="form-control" id="username" name="username" >
                 </div>
                 <div class="mb-3">
-                    <label for="new_password" class="form-label">New Password:</label>
+                    <label for="new_password" class="form-label">Password:</label>
                     <input type="password" class="form-control" id="new_password" name="new_password" required>
                 </div>
                 <div class="mb-3">
